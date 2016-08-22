392477c7ac44d2ac8a7a999fceb8a8216223b043
Allan Kinnaird <akinnaird@icloud.com>
2016-08-22 14:11:05 +0100

    Correct indentation

--- AvgMaxTempMapper.java
+++ AvgMaxTempMapper.java
@@ -6,21 +6,22 @@ import org.apache.hadoop.mapred.*;
 
 public class AvgMaxTempMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
 
-	public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter reporter) throws IOException {
-    	
+	public void map(LongWritable key, Text value, OutputCollector<Text,
+			DoubleWritable> output, Reporter reporter) throws IOException {
+		
 		// Set a couple of descriptive constants
 		int MONTH = 3;
 		int MAXTEMP = 5;
 
 		//
 		// Convert the line to a string
-    	//
+		//
 		String line = value.toString();
-        
+		
 		// Split it into its comma-delimited fields
 		//
 		String[] fields = line.split(",");
-        
+		
 		try {
 			double maxtemp = Double.parseDouble(fields[MAXTEMP]);
 			// If we have a valid temperature, we also have a valid month
@@ -33,6 +34,6 @@ public class AvgMaxTempMapper extends MapReduceBase implements Mapper<LongWritab
 		catch(NumberFormatException e) {
 			// We expect some entries to be non-numeric, so do nothing
 		}
-        
+		
 	}
 }
